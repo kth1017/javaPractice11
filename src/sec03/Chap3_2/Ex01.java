129f5d79fa5bdd3856ab07da392914d480133581
kth <rnjsxo1017@gmail.com>
2023-11-07 00:24:16 +0900

    메모1

---
+++ src/sec03/Chap3_2/Ex01.java
@@ -0,0 +1,12 @@
+package sec03.Chap3_2;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        int age = 20;
+        boolean isMarried = false;
+        double height = 179.99;
+        char sex = 'M';
+        String name = "홍길동";
+    }
+
+}
