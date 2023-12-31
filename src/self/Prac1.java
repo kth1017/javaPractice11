3b599f7258e6b1f564bd5525121b4f42a0df5453
kth <rnjsxo1017@gmail.com>
2023-12-31 23:20:40 +0900

    231231

--- src/self/Prac1.java
+++ src/self/Prac1.java
@@ -4,10 +4,19 @@ import java.math.BigDecimal;
 
 public class Prac1 {
     public static void main(String[] args) {
+        // 매개변수 args
         for (String arg : args) {
             System.out.println(arg);
         }
 
+        // 타입 추론
+        for (var i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+
+        /*
+        decimal
+         */
         float num6 = new BigDecimal("0.2")
                 .add(new BigDecimal("0.3"))
                 .floatValue();
@@ -29,7 +38,25 @@ public class Prac1 {
                 .doubleValue();
     }
 
+    // 버퍼 빌더
+
     CharSequence cs1 = "ABC";
     CharSequence cs2 = new StringBuffer();
     CharSequence cs3 = new StringBuilder();
+
+    /*
+    꼬리 재귀
+     */
+
+    static int factorial (int num) {
+        return num == 0 ? 1 : num * factorial(--num);
+    }
+
+    int fact1 = factorial(1);
+    int fact2 = factorial(2);
+    int fact3 = factorial(3);
+    int fact4 = factorial(4);
+    int fact5 = factorial(5);
+
+
 }
