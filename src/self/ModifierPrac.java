a3b9e0222b4afc16a9b735b851cc62d1fb0c3762
kth <rnjsxo1017@gmail.com>
2024-01-02 22:37:09 +0900

    240101

---
+++ src/self/ModifierPrac.java
@@ -0,0 +1,8 @@
+package self;
+
+public class ModifierPrac {
+    String powerButton = "OnOff";
+    public String sdCardSlot = "SD Card";
+    private String cpu = "Yalcom";
+    protected String prot = "qw";
+}
