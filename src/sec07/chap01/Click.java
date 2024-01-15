93e5b59bccf7f7daf7a6964a29b5995fce18e417
kth <rnjsxo1017@gmail.com>
2024-01-15 23:19:23 +0900

    240115

--- src/sec07/chap01/Click.java
+++ src/sec07/chap01/Click.java
@@ -1,2 +1,19 @@
-package sec07.chap01;public class Click {
+package sec07.chap01;
+
+public class Click {
+    int x;
+    int y;
+    int timestamp;
+
+    public Click(int x, int y) {
+        this.x = x;
+        this.y = y;
+
+    }
+
+    //  ⭐️ 아래를 주석해제하고 다시 실행해 볼 것
+    //  @Override
+    //  public int hashCode() {
+    //      return x * 100000 + y;
+    //  }
 }
---
+++ src/self/MissCasting.java
@@ -0,0 +1,36 @@
+package self;
+
+public class MissCasting {
+    public static void main(String[] args) {
+        // 자바에서 클래스 캐스팅시 거치는 메모리 과정은
+        /*
+        1 겍체의 실제 타입 확인
+        2 캐스팅 유효성 검사(상속 관게 확인)
+        3 메모리 조정
+         */
+
+        interface MyInterface {
+            void myMethod();
+        }
+
+        class MyClass implements MyInterface {
+            public void myMethod() {
+                System.out.println("Implementation of MyInterface");
+            }
+
+            void myClassMethod() {
+                System.out.println("MyClass specific method");
+            }
+        }
+
+        MyInterface myInterfaceObj = new MyClass();
+
+
+        // 잘못된 캐스팅 - 실제로는 MyClass 객체이지만 MyInterface 타입으로 캐스팅
+        // 이 시점에서는 컴파일 오류가 발생하지 않습니다.
+        MyClass myClassObj = (MyClass) myInterfaceObj;
+
+        // 실행 중에 ClassCastException 발생
+//        myClassObj.myClassMethod();
+    }
+}
